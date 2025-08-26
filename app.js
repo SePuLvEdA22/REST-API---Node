13332d20ffc7234d8e8df57cd44df19b7e1e58b0
Helger <helgerjo123@gmail.com>
2025-08-26 11:14:56 -0500

    adding post method

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const crypto = require('node:crypto')
 const movies = require('./data/movies.json')
+const { validateMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json()) // middleware que parsea el body a json
 app.disable('x-powered-by') // deshabilita el header x-powered-by; Express
 
-// app.get('/movies', (req, res) => {
-//   res.json(movies)
-// })
+app.get('/movies', (req, res) => {
+  res.json(movies)
+})
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query
@@ -15,6 +18,22 @@ app.get('/movies', (req, res) => {
       (movie) => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
     return res.json(filteredMovie)
   }
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  const newNMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+  // Esto no seria REST, porque estamos guardando
+  // el estado en memoria
+  movies.push(newNMovie)
+  res.status(201).json(newNMovie) // actualizar la cache del cliente
 })
 
 app.get('/movies/:id', (req, res) => {
