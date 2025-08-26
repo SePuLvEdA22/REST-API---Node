c2db5389492552eddb95e5e674e3f2342869e3d5
Helger <helgerjo123@gmail.com>
2025-08-25 22:01:15 -0500

    adding get methods

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
-const movies = require('./movies.json')
+const movies = require('./data/movies.json')
 
 const app = express()
 app.disable('x-powered-by') // deshabilita el header x-powered-by; Express
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hola mundo' })
-})
+// app.get('/movies', (req, res) => {
+//   res.json(movies)
+// })
 
 app.get('/movies', (req, res) => {
-  res.json(movies)
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovie = movies.filter(
+      (movie) => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+    return res.json(filteredMovie)
+  }
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
 })
 
 const PORT = process.env.PORT ?? 3000
